d1a3c96ddca0a8f966d41bde3b97a0b4b7c6f3ca
Steve Wagner <ciroque@live.com>
2018-11-12 14:47:35 -0800

    WIP, before tack change...

--- index.js
+++ index.js
@@ -8,21 +8,42 @@ const typeDefs = `
     id: ID!
     name: String!
     description: String
+    category: PhotoCategory!
+    url: String!
+    type: String!
+    postedBy: String!
+  }
+  type User {
+    githubLogin: ID!
+    name: String!
+    postedPhotos: [Photo!]!
+  }
+  enum PhotoCategory {
+    LANDSCAPE
+    PORTRAIT
   }
   type Mutation {
     postPhoto(name: String!, description: String): Photo! 
+    postUser(name: String!): User! 
   }
   type Query {
     totalPhotos: Int!
     allPhotos: [Photo!]!
+    totalUsers: Int!
+    allUsers: [User!]!
+    aPhoto(id: ID!): Photo!
   }
 `;
 
+const DEFAULT_IMAGE_TYPE = 'jpg';
 const photos = []; // just for now
 const resolvers = {
   Query: {
     totalPhotos: (parent, args, {photos}) => photos.countDocuments(),
-    allPhotos: (parent, args, {photos}) => photos.find().toArray()
+    allPhotos: (parent, args, {photos}) => photos.find().toArray(),
+    totalUsers: (parent, args, {users}) => users.countDocuments(),
+    allUsers: (parent, args, {users}) => users.find().toArray(),
+    aPhoto: (parent, {id}, {photos}) => photos.findOne({id: id})
   },
   Mutation: {
     postPhoto: async (parent, args, {photos}) => {
@@ -30,7 +51,23 @@ const resolvers = {
       const { insertedId } = await photos.insertOne(newPhoto);
       newPhoto.id = insertedId.toString();
       return newPhoto;
+    },
+    postUser: async (parent, args, {users}) => {
+      const newUser = {...args};
+      const { insertedId } = await users.insertOne(newUser);
+      newUser.id = insertedId.toString();
+      return newUser;
     }
+  },
+  Photo: {
+    id: (parent, {id}, {photos}) => parent.id || parent._id.toString(),
+    url: parent => `/img/${parent._id}.${parent.type || DEFAULT_IMAGE_TYPE}`,
+    type: (parent, {type}, {photos}) => type || "jpg",
+    category: (parent, {category}, {photos}) => category || PhotoCategory.LANDSCAPE,
+    postedBy: (parent, args, {users}) => users.findOne({githubLogin: parent._id}) || {}
+  },
+  User: {
+    postedPhotos: (parent, args, {photos}) => photos.find({userID: parent.githubLogin}).toArray()
   }
 };
 
@@ -49,7 +86,8 @@ const start = async () => {
     typeDefs,
     resolvers,
     context: {
-      photos: db.collection('photos')
+      photos: db.collection('photos'),
+      users: db.collection('users')
     }
   });
 
